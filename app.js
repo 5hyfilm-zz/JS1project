6fd7d605f6cd44e92b1ca4901cf5ac8086857d6f
Waranthorn Chansawang <waranthorn_c@outlook.com>
2020-10-13 17:43:16 +0700

    -.-

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-function calBEU(fc, sp, vc){
-    beu = fc/(sp-vc) // fc = fixed cost, sp = selling price per unit, vc = variable cost per unit
-    return beu + "บาท"
+function calBEU(){
+    var fc = document.data.fc.value
+    var sp = document.data.sp.value
+    var vc = document.data.vc.value
+    var beu = fc/(sp-vc) // fc = fixed cost, sp = selling price per unit, vc = variable cost per unit
+    return beu
 }
 
 function display(elementID, value){
     document.getElementById(elementID).innerHTML = value
 }
-// function cal(){
 
-// }
+display("result", beu)
